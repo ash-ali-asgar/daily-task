2d2032d1391a84e1b5958cfcb2f2ad66a1d2827d
ash-ali-asgar <64121092+ash-ali-asgar@users.noreply.github.com>
2024-12-03 19:23:43 +0530

    this is a just start

---
+++ app.js
@@ -0,0 +1,12 @@
+function concatenateStrings(string1, string2) {
+    return string1 + string2;
+}
+
+// Taking input from the user
+const str1 = prompt("Enter the first string:");
+const str2 = prompt("Enter the second string:");
+
+// Concatenating the strings
+const result = concatenateStrings(str1, str2);
+
+console.log(result); 
